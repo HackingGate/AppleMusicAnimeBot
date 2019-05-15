68a754e4c7abbda1a99369af36097e482903e87c
HackingGate <i@hackinggate.com>
2019-05-15 18:15:07 +0900

    Update to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "AppleMusicAnime",
     dependencies: [
-        .package(url: "https://github.com/scottrhoyt/Cider.git", from: "0.10.0"),
-        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "4.0.0")
+        .package(url: "https://github.com/HackingGate/Cider.git", from: "0.1.0"),
+        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.0.0")
     ],
     targets: [
         .target(
